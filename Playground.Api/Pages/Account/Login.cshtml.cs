64a4ae2287debf7fa44ba9fa6bd7bcfa7509fa9d
Стас <stasnocap@gmail.com>
2023-11-11 16:48:01 +0300

    working on authentication

---
+++ Playground.Api/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Playground.Api.Pages.Account;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- Playground.Api/Program.cs
+++ Playground.Api/Program.cs
@@ -1,6 +1,11 @@
+using Duende.IdentityServer.Models;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Playground.Api;
 using Playground.Api.Form.Validation;
 using Playground.Data;
 using Playground.Data.Seeders;
@@ -8,6 +13,12 @@ using Playground.Data.Seeders;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddRazorPages();
+
+AddIdentity(builder.Services);
+
+const string devCors = "dev";
 
 builder.Services.AddCors(x =>
 {
@@ -21,26 +32,83 @@ builder.Services
     .AddFluentValidationAutoValidation()
     .AddValidatorsFromAssemblyContaining<CreateToDoFormValidation>();
 
-builder.Services.AddDbContext<AppDbContext>(x =>
-{
-    x.UseInMemoryDatabase("Playground.Database");
-});
+builder.Services.AddDbContext<AppDbContext>(x => { x.UseInMemoryDatabase("Playground.Database"); });
 
 var app = builder.Build();
 
 Seed(app.Services);
 
-app.MapDefaultControllerRoute();
+app.UseCors(devCors);
 
-app.UseCors("dev");
+app.UseAuthentication();
+
+app.UseIdentityServer();
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 app.Run();
 
-static void Seed(IServiceProvider serviceProvider)
+void AddIdentity(IServiceCollection services)
+{
+    services.AddDbContext<IdentityDbContext>();
+
+    services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        {
+            if (app!.Environment.IsDevelopment())
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            }
+            else
+            {
+                // todo configure for production
+            }
+        })
+        .AddEntityFrameworkStores<IdentityDbContext>();
+
+    var identityServerBuilder = services.AddIdentityServer();
+
+    identityServerBuilder.AddAspNetIdentity<IdentityUser>();
+
+    if (app!.Environment.IsDevelopment())
+    {
+        identityServerBuilder.AddInMemoryIdentityResources(new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        });
+
+        identityServerBuilder.AddInMemoryClients(new[]
+        {
+            new Client
+            {
+                ClientId = "web-client",
+                AllowedGrantTypes = GrantTypes.Code,
+                
+                RedirectUris = new[] { "http://localhost:3000" },
+                PostLogoutRedirectUris = new[] { "http://localhost:3000" },
+                AllowedCorsOrigins = new[] { "http://localhost:3000" },
+                
+                RequirePkce = true,
+                AllowAccessTokensViaBrowser = true,
+                RequireConsent = false,
+                RequireClientSecret = false,
+            }
+        });
+
+        identityServerBuilder.AddDeveloperSigningCredential();
+    }
+}
+
+void Seed(IServiceProvider serviceProvider)
 {
     using var scope = serviceProvider.CreateScope();
     var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    
+
     var todoSeeder = new TodoSeeder();
     todoSeeder.Seed(appDbContext);
 }
